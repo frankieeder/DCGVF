3f9d30117cd3507cce805491af7f57a95f1184f3
Frankie Eder <frankieeder@Frankies-iMac.lan1>
2020-04-02 15:10:55 -0700

    working prores read.

--- databases.py
+++ databases.py
@@ -6,20 +6,23 @@ import ffmpeg
 import os
 import numpy as np
 
-data_root = '/Volumes/Elements/Camera Obscura Backup Footage/'
+data_root = '/Volumes/Backup_Elem/Footage_Proxies_Old/'
 all_files = []
 for root, folders, files in os.walk(data_root):
-    all_files.extend([root + f for f in files if f.endswith('.R3D')])
+    all_files.extend([os.path.join(root, f) for f in files if f.endswith('.mp4')])
 #print(sum([root + f for f in files if f.endswith('.R3D')] for root, folders, files in os.walk(data_root)))
 print(all_files[0])
 
 out, err = (
     ffmpeg
     .input(all_files[0])
-    .output('pipe:', format='rawvideo', pix_fmt='rgb24')
+    .output('pipe:', format='rawvideo', pix_fmt='rgb48', vf="select='between(n\,0\,7)'", vsync=0)
     .run(capture_stdout=True)
 )
-print(out)
+#print(out)
+video = np.frombuffer(out, np.uint16).reshape([-1, 720, 864, 3])
+print(video.shape)
+print(video.min(), video.max())
 #class R3DVideoDataset(Dataset):
 
 
