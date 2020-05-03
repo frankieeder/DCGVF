6eb7eb1982a578fd65e6d271d8a6b50877409db5
Frankie Eder <frankieeder@Frankies-iMac.lan1>
2020-05-02 17:22:30 -0700

    Some changes from desktop, porting to laptop for eventual use with colab.

--- databases.py
+++ databases.py
@@ -6,13 +6,14 @@ import ffmpeg
 import os
 import numpy as np
 
-data_root = '/Volumes/Backup_Elem/Footage_Proxies_Old/'
+data_root = '/Volumes/Elements/Camera Obscura Backup Footage/'
 all_files = []
 for root, folders, files in os.walk(data_root):
-    all_files.extend([os.path.join(root, f) for f in files if f.endswith('.mp4')])
+    all_files.extend([os.path.join(root, f) for f in files if f.endswith('001.mov')])
 #print(sum([root + f for f in files if f.endswith('.R3D')] for root, folders, files in os.walk(data_root)))
 print(all_files[0])
-
+height, width = 720, 864
+info = ffmpeg.probe(all_files[0])
 out, err = (
     ffmpeg
     .input(all_files[0])
@@ -20,10 +21,47 @@ out, err = (
     .run(capture_stdout=True)
 )
 #print(out)
-video = np.frombuffer(out, np.uint16).reshape([-1, 720, 864, 3])
+video = np.frombuffer(out, np.uint16).reshape([-1, height, width, 3])
 print(video.shape)
+
 print(video.min(), video.max())
 #class R3DVideoDataset(Dataset):
+
+
+class VideoFrame(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, csv_file, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.landmarks_frame = pd.read_csv(csv_file)
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.landmarks_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.landmarks_frame.iloc[idx, 0])
+        image = io.imread(img_name)
+        landmarks = self.landmarks_frame.iloc[idx, 1:]
+        landmarks = np.array([landmarks])
+        landmarks = landmarks.astype('float').reshape(-1, 2)
+        sample = {'image': image, 'landmarks': landmarks}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
 
 
 
