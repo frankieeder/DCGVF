eb3ef6292397d69e03f18b559246938bddbaf971
Frankie Eder <frankieeder@Frankies-iMac.local>
2020-05-04 20:23:51 -0700

    COOL ARTISTIC STYLE IMAGE DIFF STORED IN variable a

--- databases.py
+++ databases.py
@@ -5,6 +5,7 @@ import torchvision
 import ffmpeg
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 data_root = '/Volumes/Elements/Camera Obscura Backup Footage/'
 all_files = []
@@ -12,16 +13,38 @@ for root, folders, files in os.walk(data_root):
     all_files.extend([os.path.join(root, f) for f in files if f.endswith('001.mov')])
 #print(sum([root + f for f in files if f.endswith('.R3D')] for root, folders, files in os.walk(data_root)))
 print(all_files[0])
-height, width = 720, 864
+height, width = 1322, 1102
 info = ffmpeg.probe(all_files[0])
+w, h = info['streams'][0]['width'], info['streams'][0]['height']
+
+x=2
+'''out, _ = (
+    ffmpeg
+    .input(all_files[0])
+    .filter('select', 'gte(n,{})'.format(1))
+    .output('pipe:', vframes=1, format='image2', vcodec='mjpeg')
+    .run(capture_stdout=True)
+)'''
+out, err = (
+    ffmpeg
+    .input(all_files[0])
+    .output('pipe:', format='rawvideo', pix_fmt='rgb24', vf="select='between(n\,0\,7)'")#, **{'c:v': 'libx264'})#, '-b:v': '200k'})
+    .run(capture_stdout=True)
+)
+#print(out)
+x=2
+video_h264 = np.frombuffer(out, np.uint8).reshape([-1, h, w, 3])
+x=2
 out, err = (
     ffmpeg
     .input(all_files[0])
     .output('pipe:', format='rawvideo', pix_fmt='rgb48', vf="select='between(n\,0\,7)'", vsync=0)
     .run(capture_stdout=True)
 )
-#print(out)
-video = np.frombuffer(out, np.uint16).reshape([-1, height, width, 3])
+video_prores = np.frombuffer(out, np.uint16).reshape([-1, h, w, 3])
+a = (video_prores / (2**16)) - (video_h264 / (2**8))
+a -= a.min()
+a /= a.max()
 print(video.shape)
 
 print(video.min(), video.max())
